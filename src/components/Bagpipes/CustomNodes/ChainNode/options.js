1b295ce5479df123218592917c8a69aa0006cf04
Filip <flipchan@riseup.net>
2023-09-11 20:26:15 +0200

    remote fetch assets

---
+++ src/components/Bagpipes/CustomNodes/ChainNode/options.js
@@ -0,0 +1,61 @@
+import { list_assethub_assets, list_hydradx_assets } from './../../../Chains/draft_tx';
+
+
+export const chainOptions = [
+    {
+      value: 'polkadot',
+      label: 'Polkadot',
+      logo: './polkadot-logo.svg',
+    },
+    {
+      value: 'rococo',
+      label: 'Rococo',
+      logo: './polkadot-logo.svg'
+    },
+    {
+      value: 'hydradx',
+      label: 'Hydra',
+      logo: './hydra-logo.svg'
+    },
+    {
+      value: 'assetHub',
+      label: 'Asset Hub',
+      logo: './assethub-logo.svg'
+    },
+  ];
+
+
+  export const assetOptions = [
+    {
+      chain: 'polkadot',
+      assets: [
+        {
+          name: 'polkadotDot',
+          ticker: 'DOT',
+          description: 'Polkadot'
+        }
+      ]
+    },
+    {
+      chain: 'rococo',
+      assets: [
+        {
+          name: 'rococoRoc',
+          ticker: 'ROC',
+          description: 'Rococo'
+        }
+      ]
+    },
+    {
+      chain: 'hydradx',
+      assets: 
+      await list_hydradx_assets()
+    },
+    {
+      chain: 'assetHub',
+      assets: 
+        await list_assethub_assets()
+      
+    }
+  ];
+
