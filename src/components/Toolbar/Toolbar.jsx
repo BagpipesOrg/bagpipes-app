4c46aec98de6b18b77423557ca032cdc7137be01
Decentration <45230082+decentration@users.noreply.github.com>
2023-11-20 16:52:03 +0000

    add buttons

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Tippy from '@tippyjs/react';
 import ThemeContext from '../../contexts/ThemeContext';
-import { ActionIcon, ChainIcon, ToolsIcon } from '../Icons/icons'; 
+import { ActionIcon, ChainIcon, ToolsIcon, RouterIcon, WebhookIcon, ScheduleIcon, DelayIcon } from '../Icons/icons'; 
 import 'tippy.js/dist/tippy.css'; 
 import './Toolbar.scss'; 
 
@@ -21,27 +21,50 @@ const nodeNames = {
     chain: 'Chain',
     action: 'Action',
     tools: 'Tools',
+    webhook: 'Webhook',
+    router: 'Router',
+    schedule: 'Schedule',
+    delay: 'Delay',
 };
 
 const nodeDescriptions = {
   chain: 
-  <div className='m-4 tippy-node'>
-    <h1 className='text-xl font-bold'>Chain </h1>
-    <p>Drag and drop a chain node on to the canvas to select the chain you want to use.</p>
-<img src='./ChainNodeScreenshot.png'></img>
-  </div>,
+    <div className='m-4 tippy-node'>
+        <h1 className='text-xl font-bold'>Chain </h1>
+        <p>Drag and drop a chain node on to the canvas to select the chain you want to use.</p>
+        <img src='./ChainNodeScreenshot.png'></img>
+    </div>,
   action: 
-  <div className='m-4 tippy-node'>
-    <h1 className='text-xl font-bold'>Action </h1>
-    <p>Drag and drop an action to make an action (transfer, xTransfer, Swap, etc.).</p>
-<img className='action-image'src='./ActionNodeScreenshot.png'></img>
-  </div>,
+    <div className='m-4 tippy-node'>
+        <h1 className='text-xl font-bold'>Action </h1>
+        <p>Drag and drop an action to make an action (transfer, xTransfer, Swap, etc.).</p>
+        <img className='action-image'src='./ActionNodeScreenshot.png'></img>
+    </div>,
    tools: 
-   <div className='m-4 tippy-node'>
-     <h1 className='text-xl font-bold '>Tools (coming soon) </h1>
-     <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
-
-   </div>
+    <div className='m-4 tippy-node'>
+      <h1 className='text-xl font-bold '>Tools (coming soon) </h1>
+      <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
+    </div>,
+  webhook: 
+    <div className='m-4 tippy-node'>
+      <h1 className='text-xl font-bold '>Webhook (coming soon) </h1>
+      <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
+  </div>,
+  router: 
+    <div className='m-4 tippy-node'>
+      <h1 className='text-xl font-bold '>Router (coming soon) </h1>
+      <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
+  </div>,
+  schedule: 
+  <div className='m-4 tippy-node'>
+    <h1 className='text-xl font-bold '>Schedule (coming soon) </h1>
+    <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
+</div>,
+delay: 
+<div className='m-4 tippy-node'>
+  <h1 className='text-xl font-bold '>Delay (coming soon) </h1>
+  <p>A list of tools to add more functionality to your workflow, such as event handlers, if/else statements, promises, for loops, hash functions, routers, triggers, etc.</p>
+</div>,
 }
 
 const Toolbar = () => {
@@ -51,6 +74,37 @@ const Toolbar = () => {
     event.dataTransfer.effectAllowed = 'move';
   };
 
+  const ToolsPopoverContent = () => {
+    return (
+        <div className='flex flex-col'>
+            <Tippy content={nodeDescriptions.webhook}>
+                <div className={`toolbar-icon ${theme}`} onDragStart={(event) => onDragStart(event, 'webhook')} draggable>
+                    <WebhookIcon />
+                    <span>Webhook</span>
+                </div>
+            </Tippy>
+            <Tippy content={nodeDescriptions.router}>
+                <div className={`toolbar-icon ${theme}`} onDragStart={(event) => onDragStart(event, 'router')} draggable>
+                    <RouterIcon />
+                    <span>Router</span>
+                </div>
+            </Tippy>
+            <Tippy content={nodeDescriptions.router}>
+                <div className={`toolbar-icon ${theme}`} onDragStart={(event) => onDragStart(event, 'delay')} draggable>
+                    <DelayIcon />
+                    <span>Delay</span>
+                </div>
+            </Tippy>
+            <Tippy content={nodeDescriptions.router}>
+                <div className={`toolbar-icon ${theme}`} onDragStart={(event) => onDragStart(event, 'schedule')} draggable>
+                    <ScheduleIcon />
+                    <span>Schedule</span>
+                </div>
+            </Tippy>
+        </div>
+    );
+};
+
   const renderNode = (nodeKey) => {
     let IconComponent;
     if (nodeKey === 'chain') {
@@ -59,7 +113,27 @@ const Toolbar = () => {
       IconComponent = <ActionIcon />;
     } else if (nodeKey === 'tools') {
       IconComponent = <ToolsIcon />;
+    // } else if (nodeKey === 'tools') {
+    //   return (
+    //       <Tippy placement="bottom" theme="light" interactive={true} content={<ToolsPopoverContent />}>
+    //           <div className={`toolbar-icon ${theme}`}>
+    //               <ToolsIcon />
+    //               <span>{nodeNames[nodeKey]}</span>
+    //           </div>
+    //       </Tippy>
+    //   );
+  } else if (nodeKey === 'router') {
+      IconComponent = <RouterIcon />;
+    } else if (nodeKey === 'webhook') {
+      IconComponent = <WebhookIcon />;
+    } else if (nodeKey === 'schedule') {
+      IconComponent = <ScheduleIcon />;
+    } else if (nodeKey === 'delay') {
+      IconComponent = <DelayIcon />;
     }
+
+
+    
 
     return (
       <Tippy  placement="bottom" theme="light" className='tippy-node' interactive={true} content={nodeDescriptions[nodeKey]}>
