e835580fe49277113d8b9e15e3b1c753164430e5
Filip <flipchan@riseup.net>
2023-11-28 21:28:21 +0100

    short links works

--- src/components/Bagpipes/TemplateFeatures/compress.ts
+++ src/components/Bagpipes/TemplateFeatures/compress.ts
@@ -2,8 +2,11 @@
 //var URLSafeBase64 = require('urlsafe-base64');
 import * as pako from 'pako';
 
+import { saveUrl, getUrl } from './handledb';
 
-export function compressString(input: string): string {
+
+export async function compressString(input: string): Promise<string> {
+  console.log(`compressing string`)
   try {
     const utf8encoded = new TextEncoder().encode(input);
     const compressed = pako.deflate(utf8encoded);
@@ -11,7 +14,11 @@ export function compressString(input: string): string {
 
     console.log('Base64 Encoded:', base64encoded);
 
-    return base64encoded;
+    
+    const shortUrl = await saveUrl(base64encoded);
+    console.log('saved link: ', shortUrl);
+
+    return shortUrl;
   } catch (error) {
     console.error('Error compressing string:', error);
     return ''; // Return an empty string or handle the error as needed
@@ -25,9 +32,16 @@ function addPadding(base64String: string): string {
   return base64String;
 }
 
-  export function decompressString(compressedInput: string): string {
+  export async function decompressString(compressedInput: string): Promise<string> {
+    console.log(`decompressing string`);
+    console.log(`got input:`, compressedInput);
     try {
-      const in2 = addPadding(compressedInput);
+      const expandedUrl = await getUrl(compressedInput);
+      console.log(`0 expanded url:`, expandedUrl.url);
+      console.log(`expanded url:`, expandedUrl);
+      const newinput = expandedUrl.url;
+      console.log(`newinput:`, newinput);
+      const in2 = addPadding(newinput);
       // Ensure that the base64 string is properly formatted
       const formattedInput = in2.replace(/[^A-Za-z0-9+/]/g, '');
 
@@ -40,6 +54,8 @@ function addPadding(base64String: string): string {
       );
 
       const decompressed = pako.inflate(uint8Array, { to: 'string' });
+      console.log(`calling get url`);
+
       return decompressed;
     } catch (error) {
       console.error('Error decoding base64 string:', error);
