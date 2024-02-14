24bc0d404308c23f947b596e720fee2b2e49449f
Decentration <45230082+decentration@users.noreply.github.com>
2024-02-14 10:20:13 +0000

    update wasm-crypto

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     })  
   ],
 optimizeDeps: {
+  include: ['wasm-crypto'],
         esbuildOptions: {
             // Node.js global to browser globalThis
             define: {
